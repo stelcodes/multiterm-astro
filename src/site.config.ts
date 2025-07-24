28575973c48f4e24dfecc14444380f6f70b53168
hamza-m-masood <hamzamasood183@gmail.com>
2025-07-24 22:30:44 +0100

    select theme

--- src/site.config.ts
+++ src/site.config.ts
@@ -35,7 +35,7 @@ const config: SiteConfig = {
   // The theming configuration for the site.
   themes: {
     // The theming mode. One of "single" | "select" | "light-dark-auto".
-    mode: 'light-dark-auto',
+    mode: 'select',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
     default: 'catppuccin-macchiato',
